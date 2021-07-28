81fe5a3f1e8298326bd7107977fa7f61a04f52a3
danielbossier <dbossier21@gmail.com>
2021-07-28 13:01:58 -0700

    updated teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,9 +7,12 @@ class TeamsController < ApplicationController
   def create
     team = Team.new(
       name: params[:name],
+      city: params[:city],
       wins: params[:wins],
       losses: params[:losses],
       ties: params[:ties],
+      conference: params[:conference],
+      division: params[:division],
       league_id: params[:league_id],
       user_id: current_user.id,
     )
@@ -25,9 +28,12 @@ class TeamsController < ApplicationController
   def update
     team = Team.find_by(id: params[:id])
     team.name = params[:name] || team.name
+    team.city = params[:city] || team.city
     team.wins = params[:wins] || team.wins
     team.losses = params[:losses] || team.losses
     team.ties = params[:ties] || team.ties
+    team.conference = params[:conference] || team.conference
+    team.division = params[:division] || team.division
     team.league_id = params[:league_id] || team.league_id
     team.save
     render json: team.as_json
