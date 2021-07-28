59a90b67a7ad957e0cfb237ed82c971106c6c345
danielbossier <dbossier21@gmail.com>
2021-07-28 12:29:00 -0700

    added all teams, added city column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_185900) do
+ActiveRecord::Schema.define(version: 2021_07_28_191932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_185900) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "division"
     t.string "conference"
+    t.string "city"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,65 @@
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # group = Group.create!(name: "HOFers")
 
 # user = User.create!(username: "Shoeless Joe", group_id: 1, email: "joe@example.com")
 
 # league = League.create!(league: "MLB")
 
-# team = Team.create!(name: "White Sox", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "White Sox", city: "Chicago", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "AL")
 
-# team = Team.create!(name: "Braves", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Guardians", city: "Cleveland", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "AL")
 
-# team = Team.create!(name: "Marlins", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Tigers", city: "Detroit", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "AL")
 
-# team = Team.create!(name: "Mets", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Royals", city: "Kansas City", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "AL")
 
-# team = Team.create!(name: "Phillies", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Twins", city: "Minnesota", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "AL")
 
-# team = Team.create!(name: "Nationals", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Orioles", city: "Baltimore", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "AL")
 
-# team = Team.create!(name: "Cubs", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Red Sox", city: "Boston", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "AL")
 
-# team = Team.create!(name: "Reds", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Yankees", city: "New York", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "AL")
 
-# team = Team.create!(name: "Brewers", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Rays", city: "Tampa Bay", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "AL")
 
-# team = Team.create!(name: "Pirates", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Blue Jays", city: "Toronto", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "AL")
 
-# team = Team.create!(name: "Cardinals", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+# team = Team.create!(name: "Astros", city: "Houston", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "AL")
+
+# team = Team.create!(name: "Angels", city: "Los Angeles", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "AL")
+
+# team = Team.create!(name: "Athletics", city: "Oakland", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "AL")
+
+# team = Team.create!(name: "Mariners", city: "Seattle", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "AL")
+
+# team = Team.create!(name: "Rangers", city: "Texas", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "AL")
+
+# team = Team.create!(name: "Braves", city: "Atlanta", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "NL")
+
+# team = Team.create!(name: "Marlins", city: "Miami", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "NL")
+
+# team = Team.create!(name: "Mets", city: "New York", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "NL")
+
+# team = Team.create!(name: "Phillies", city: "Philadelphia", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "NL")
+
+# team = Team.create!(name: "Nationals", city: "Washington", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "East", conference: "NL")
+
+# team = Team.create!(name: "Diamondbacks", city: "Arizona", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "NL")
+
+# team = Team.create!(name: "Rockies", city: "Colorado", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "NL")
+
+# team = Team.create!(name: "Dodgers", city: "Los Angeles", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "NL")
+
+# team = Team.create!(name: "Padres", city: "San Diego", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "NL")
+
+# team = Team.create!(name: "Giants", city: "San Francisco", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "West", conference: "NL")
+
+# team = Team.create!(name: "Cubs", city: "Chicago", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "NL")
+
+# team = Team.create!(name: "Reds", city: "Cincinnati", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "NL")
+
+# team = Team.create!(name: "Brewers", city: "Milwaukee", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "NL")
+
+# team = Team.create!(name: "Pirates", city: "Pittsburgh", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "NL")
+
+# team = Team.create!(name: "Cardinals", city: "St. Louis", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1, division: "Central", conference: "NL")
