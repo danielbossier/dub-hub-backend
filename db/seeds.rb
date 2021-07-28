0ed9a007de75f38da189e5de67fa431ea984ec8b
danielbossier <dbossier21@gmail.com>
2021-07-28 09:36:33 -0700

    added most NL teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,23 @@
 # league = League.create!(league: "MLB")
 
 # team = Team.create!(name: "White Sox", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Braves", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Marlins", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Mets", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Phillies", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Nationals", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Cubs", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Reds", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Brewers", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Pirates", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
+
+# team = Team.create!(name: "Cardinals", wins: 10, losses: 5, ties: 0, league_id: 1, user_id: 1)
