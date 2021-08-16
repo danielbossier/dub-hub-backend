c1c89179caa62f42404119b1708b38ca6dd84a35
danielbossier <dbossier21@gmail.com>
2021-08-16 10:26:39 -0700

    removed user_id from teams model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_205551) do
+ActiveRecord::Schema.define(version: 2021_08_16_172451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2021_08_10_205551) do
     t.integer "losses"
     t.integer "ties"
     t.integer "league_id"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "division"
