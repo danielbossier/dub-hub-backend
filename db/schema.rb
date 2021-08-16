eaa477233c1a674d6d6ae0ae67109c84ec780a4e
danielbossier <dbossier21@gmail.com>
2021-08-16 10:49:30 -0700

    removed group_id from users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_172451) do
+ActiveRecord::Schema.define(version: 2021_08_16_174801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_172451) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.integer "group_id"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
