12b6755ed385af42188ff956861a6141466e5a81
danielbossier <dbossier21@gmail.com>
2021-08-16 11:38:14 -0700

    added associations in serializers

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,6 +1,7 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :name, :wins, :losses
 
-  belongs_to :user
   belongs_to :league
+  has_many :user_teams
+  has_many :users, through: :user_teams
 end
