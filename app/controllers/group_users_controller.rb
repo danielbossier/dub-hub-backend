923044a8f89269a4018b7b199335e75db5d19a42
danielbossier <dbossier21@gmail.com>
2021-08-16 14:53:44 -0700

    added index and create actions to group_users, team_users

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,2 +1,18 @@
 class GroupUsersController < ApplicationController
+  def index
+    group_users = Group_user.all
+    render json: group_users
+  end
+
+  def create
+    group_user = Group_user.new(
+      user_id: current_user.id,
+      group_id: params[:group_id],
+    )
+    if group_user.save
+      render json: group_user
+    else
+      render json: { errors: group_user.errors.full_messages }, status: 422
+    end
+  end
 end
--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -1,4 +1,18 @@
 class TeamUsersController < ApplicationController
   def index
+    team_users = TeamUsers.all
+    render json: team_users
+  end
+
+  def create
+    team_users = TeamUsers.new(
+    user_id: current_user.id,
+    team_id: params[:team_id]
+    )
+    if team_users.save
+      render json: team_users
+    else
+      render json: {errors: team_users.errors.full_messages}, status: 422
   end
 end
+
