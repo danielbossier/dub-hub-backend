26c104b4bff40741194068cdbc5afe292186fece
danielbossier <dbossier21@gmail.com>
2021-08-17 13:02:01 -0700

    added show to users controller

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,8 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :teams
 
   has_many :group_users
-  has_many :groups, through: :group_users
-  has_many :leagues, through: :teams
   has_many :team_users
+  has_many :groups, through: :group_users
+  # has_many :leagues, through: :teams
   has_many :teams, through: :team_users
 end
