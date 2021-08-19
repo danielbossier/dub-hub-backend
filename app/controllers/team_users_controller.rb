45ef446c28748c62d71924ab7f4576064fcf3681
danielbossier <dbossier21@gmail.com>
2021-08-19 12:14:58 -0700

    added destroy method in team_users controller

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -15,4 +15,11 @@ class TeamUsersController < ApplicationController
       render json: { errors: team_user.errors.full_messages }, status: 422
     end
   end
+
+  def destroy
+    team_users_id = params["id"]
+    team_users = TeamUser.find_by(id: team_users_id)
+    team_users.destroy
+    render json: { message: "You have removed this team." }
+  end
 end
